309413b35d608bd081d705af9ea9c94b370d5de0
Sipho Yawe <yawephillip@gmail.com>
2025-12-06 20:46:31 +0000

    chore(workflow): update component barrel exports
    - Export WorkflowCard, ReadOnlyGraph, ExecutionHistory, ActivityTimeline

--- frontend/src/components/workflow/index.ts
+++ frontend/src/components/workflow/index.ts
@@ -1,3 +1,7 @@
 export { nodeTypes } from "./nodes";
 export { NLInput } from "./NLInput";
 export { WorkflowCanvas } from "./WorkflowCanvas";
+export { WorkflowCard } from "./WorkflowCard";
+export { ReadOnlyGraph } from "./ReadOnlyGraph";
+export { ExecutionHistory } from "./ExecutionHistory";
+export { ActivityTimeline } from "./ActivityTimeline";
